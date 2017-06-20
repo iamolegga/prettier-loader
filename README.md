6adfcb92f632b234f5c01c903efc1ef0bfd43f45
Oleg Repin <oleg.repin@iqoption.com>
2017-06-20 20:57:12 +0300

    fixes, sync working &  docs

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # prettier-loader
 
-Prettier loader for webpack
+Prettier loader for Webpack.
+
+## Installation
+
+```
+npm install prettier-loader prettier --save-dev
+```
+
+## Usage
+
+Now supports only Webpack 2.
+Create an issue on pr, if you need to support Webpack 1.
 
 ```js
 // webpack.config.js
@@ -10,6 +21,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        enforce: 'pre',
         loader: 'prettier-loader',
         options: { /* prettier options */ }
       }
--- index.js
+++ index.js
@@ -1,29 +1,28 @@
+const fs = require("fs");
 const path = require("path");
 const prettier = require("prettier");
 const loaderUtils = require("loader-utils");
 
-module.exports = function (source) {
+module.exports = function (source, map) {
   this.cacheable();
+  const callback = this.async();
 
   if (!(new RegExp(this.query.test)).test(this.context)) {
-    return source;
+    return callback(null, source, map);
   }
 
-  const callback = this.async();
   const prettierSource = prettier.format(
     source,
     loaderUtils.getOptions(this) || {}
   );
 
   if (prettierSource !== source) {
-    fs.writeFile(this.resourcePath, prettierSource, error => {
-      if (error) {
-        return callback(error);
-      }
-      callback(null, prettierSource);
-    });
-    return;
+    try {
+      fs.writeFileSync(this.resourcePath, prettierSource);
+    } catch (error) {
+      return callback(error);
+    }
   }
 
-  callback(null, source);
+  callback(null, prettierSource, map);
 };
